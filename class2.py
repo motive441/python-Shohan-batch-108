64bccd160014458e4ad4eaa91a9d41b762ed6a28
motive441 <motive441@gmail.com>
2022-10-03 22:58:11 +0600

    my first commit

---
+++ class2.py
@@ -0,0 +1,19 @@
+from platform import architecture
+
+
+x = '5'
+#print(type(x))
+
+# a = 5
+# A = 4
+# print(a + A)
+
+# oparetor
+# a  = 3
+# a -= 4
+# print(a)
+a = 10
+if a>10 and a<14:
+    print('Ok')
+else:
+    print('Not Ok')
