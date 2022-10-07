cd9cb2af732652fdadff97783274f26373242d93
motive441 <motive441@gmail.com>
2022-10-07 22:46:35 +0600

    class3 practice

---
+++ class3.py
@@ -0,0 +1,39 @@
+# a = 3
+# if a == 5 0r a == 3:
+# print(a)
+
+# my_tuple =  ('Onion', 'potato', 'ginger')
+
+# print(my_tuple)
+
+# for a in range(0,3,1):
+#     print(my_tuple[a])
+
+my_tuple =  ('Onion', 'potato', 'ginger', 'Onion', 'potato', 'ginger', 'Onion', 'potato', 'ginger')
+
+# print(my_tuple)
+
+# print(len(my_tuple))
+
+# for a in my_tuple:
+#     print(a)
+
+# print(my_tuple.count('potato'))
+
+my_set =  {'Onion', 'potato', 'ginger'}
+
+# print(my_set)
+
+# print(my_set[1])
+
+# my_set.add('apple')
+
+# my_set.remove('potato')
+# my_set.discard('apple')
+
+# print(my_set)
+
+'''
+for s in my_set:
+    print(s)
+'''
